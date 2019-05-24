5394475070d0fa3a4f809c8c1fdf1fc1567840a4
Amine Ben Asker <ben.asker.amine@gmail.com>
2019-05-24 23:21:09 +0200

    change stream chunk variable name

--- log_analyzer/streamer.py
+++ log_analyzer/streamer.py
@@ -9,7 +9,7 @@ class Streamer:
             # Std in file descriptor
             file_path = 0
         self.fd = open(file_path, 'r', encoding="utf-8")
-        self.chunk_size = config.get("parser.chunk_size")
+        self.chunk_size = config.get("parser.chunk")
 
     def read_chunk(self, chunk_size=None):
         if not chunk_size:
